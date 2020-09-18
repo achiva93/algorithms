cb64c11fa83b8778ad3285a7c7d8185580dcc658
Ionut Spalatelu <ionut.spalatelu@gohenry.co.uk>
2020-09-18 10:23:20 +0300

    OOP

--- src/com/lseg/classes/Main.java
+++ src/com/lseg/classes/Main.java
@@ -4,29 +4,29 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Table gabiDesk = new Table(); //Created a custom data type
+        Table desk = new Table(); //Created a custom data type
         //classes are developer defined data type
         //Table() --> constructor invocation, can be invoked only with `new` keyword
 
-        System.out.println("Gabi's desk color before setting explicit values: " + gabiDesk.color + ", legs: " + gabiDesk.getLegNumber() + ", height:" + gabiDesk.height);
+        System.out.println("Gabi's desk color before setting explicit values: " + desk.color + ", legs: " + desk.getLegNumber() + ", height:" + desk.height);
 
 
-        gabiDesk.color = "black"; //attributes are called only with name, without parentheses
-        gabiDesk.setLegNumber(4);
-        gabiDesk.height = 70;
-        gabiDesk.lentgh = 120; //cm
-        gabiDesk.width = 60; //cm
+        desk.color = "black"; //attributes are called only with name, without parentheses
+        desk.setLegNumber(4);
+        desk.height = 70;
+        desk.lentgh = 120; //cm
+        desk.width = 60; //cm
 
-        Table gabiTableKitchen = new Table();
-        gabiTableKitchen.color = "white";
-        gabiTableKitchen.setLegNumber(4);
-        gabiTableKitchen.height = 80;
-        gabiTableKitchen.lentgh = 200; //cm
-        gabiTableKitchen.width = 70; //cm
+        Table kitchenTable = new Table();
+        kitchenTable.color = "white";
+        kitchenTable.setLegNumber(4);
+        kitchenTable.height = 80;
+        kitchenTable.lentgh = 200; //cm
+        kitchenTable.width = 70; //cm
 
-        System.out.println("Gabi's desk color: " + gabiDesk.color + ", legs: " + gabiDesk.getLegNumber() + ", height:" + gabiDesk.height);
+        System.out.println("Gabi's desk color: " + desk.color + ", legs: " + desk.getLegNumber() + ", height:" + desk.height);
         System.out.println(
-                "Gabi's kitchen table color: " + gabiTableKitchen.color + ", legs: " + gabiTableKitchen.getLegNumber() + ", height:" + gabiTableKitchen.height);
+                "Gabi's kitchen table color: " + kitchenTable.color + ", legs: " + kitchenTable.getLegNumber() + ", height:" + kitchenTable.height);
 
 //        boolean hasLegNumberBeenSet = gabiDesk.setLegNumber(100);
 //        if(hasLegNumberBeenSet){
